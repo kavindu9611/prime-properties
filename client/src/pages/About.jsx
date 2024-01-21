0aec9fb0be7b7550465024724b39c9451fa34ff6
kavindu09611 <kavinduwickramasinghe1920@gmail.com>
2024-01-21 22:57:36 +0530

    Complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,20 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        Prime properties
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Prime properties stands as a premier real estate agency, committed to
+        excellence in facilitating clients through the seamless processes of
+        buying, selling, and renting properties in highly sought-after
+        neighborhoods. Our seasoned team of professionals is devoted to
+        delivering exceptional service, ensuring a smooth and gratifying
+        experience in the dynamic realm of real estate transactions. At Prime
+        properties, we take pride in our expertise and dedication, offering
+        unparalleled support to our clients as they navigate the exciting
+        journey of property acquisition and sales
+      </p>
+    </div>
+  );
 }
